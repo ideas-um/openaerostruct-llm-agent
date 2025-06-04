2cbd2b077e2c2c2a4b117e0a8f7eec72b0666aa5
Conan Lee <60785865+ConanLee918@users.noreply.github.com>
2025-06-04 13:39:05 +0800

    Added RAG module

---
+++ RAG_Embedding.py
@@ -0,0 +1,34 @@
+import os
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_chroma import Chroma
+from langchain_together import TogetherEmbeddings
+
+file = "./LLM_OpenAeroStruct/RAG_PDF/Martins--aircraft_design_metabook.pdf"
+path = os.path.abspath(file)
+
+loader = PyPDFLoader(path)
+
+docs = loader.load()
+
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000, chunk_overlap=200, add_start_index=True, length_function=len
+)
+all_splits = text_splitter.split_documents(docs)
+
+# The splits are already Document objects
+documents = all_splits
+
+# Import the embeddings
+embeddings = TogetherEmbeddings(
+    model="togethercomputer/m2-bert-80M-8k-retrieval",
+    api_key= os.environ.get('TOGETHER_AI_API_KEY')
+)
+
+# Chroma is the vector database
+vector_store = Chroma(
+    embedding_function=embeddings,
+    persist_directory='./LLM_OpenAeroStruct/Embeddings_Persist'
+)
+
+vector_store.add_documents(documents=documents)
---
+++ RAG_Retrieval.py
@@ -0,0 +1,20 @@
+#Analyze the results with RAG of the Martins Metabook#
+from langchain_chroma import Chroma
+from langchain_together import TogetherEmbeddings
+import os
+
+# Import the embeddings
+embeddings = TogetherEmbeddings(
+    model="togethercomputer/m2-bert-80M-8k-retrieval",
+    api_key= os.environ.get('TOGETHER_AI_API_KEY')
+)
+
+persist_directory = './Embeddings_Persist'
+vectordb = Chroma(persist_directory=persist_directory, embedding_function=embeddings)
+
+retriever = vectordb.as_retriever(search_type="mmr",
+    search_kwargs={'k': 5, 'fetch_k': 50})
+
+def run_retriever(query):
+    output = retriever.invoke(query)
+    return output
