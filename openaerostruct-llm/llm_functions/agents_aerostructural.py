37bcb5e3eacf6b3041fbd3116d91e9263dd4ea61
Conan Lee <conanlee918@gmail.com>
2026-02-03 23:37:56 +0800

    Updated README and base agents

--- openaerostruct-llm/llm_functions/agents_aerostructural.py
+++ openaerostruct-llm/llm_functions/agents_aerostructural.py
@@ -13,7 +13,7 @@ from ollama_config import get_model as get_ollama_model
 from google.genai import types
 
 gemini_model = "gemma-3-27b-it"
-advanced_gemini_model = "gemma-3-27b-it" #"gemini-2.5-flash" #temporarily changed back to 27b due to limit issues
+advanced_gemini_model = "gemini-2.5-flash" #"gemini-2.5-flash" #temporarily changed back to 27b due to limit issues
 
 def extract_json_between_markers(llm_output):
     # Regular expression pattern to find JSON content between ```json and ```
